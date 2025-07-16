85017ab993e2d06edd3c8e6551dda29cd33094e5
Alan Zagi <alanzagi2007@gmail.com>
2025-07-16 23:01:54 +0700

    meambahkan seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Alan',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('@@AlanZagi123##'),
+        ]);
+    }
+}
