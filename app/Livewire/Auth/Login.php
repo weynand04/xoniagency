8e752e8735d11c59c7425eb2742412b15f465e23
Alan Zagi <alanzagi2007@gmail.com>
2025-07-22 09:10:06 +0700

    update

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -39,6 +39,6 @@ class Login extends Component
 
     public function render()
     {
-        return view('livewire.auth.login');
+        return view('livewire.auth.login')->layout('layouts.guest');
     }
 }
